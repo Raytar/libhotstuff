f59509e65276b3b80694b741bc376f19603b14b7
John Ingve Olsen <johningveolsen@gmail.com>
2020-05-14 18:41:03 +0200

    updated scripts

--- scripts/run_multiple_clients.sh
+++ scripts/run_multiple_clients.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "$#" -lt 5 ]; then
-	echo "Usage: $0 [num clients] [hostname] [client id] [request rate] [exit after]"
+	echo "Usage: $0 [num clients] [hostname] [client id] [request rate] [max-async] [exit after]"
 	exit 1
 fi
 
@@ -9,14 +9,15 @@ num_clients="$1"
 hostname="$2"
 client_id="$3"
 request_rate="$4"
-exit_after="$5"
+max_async="$5"
+exit_after="$6"
 
 for (( i = 0 ; i < $num_clients ; i++ )); do
 	id=$(( client_id * num_clients + (i - num_clients) ))
 	./hotstuff-client \
 	--cid "$id" \
 	--iter -1 \
-	--max-async 3200 \
+	--max-async "$max_async" \
 	--request-rate "$request_rate" \
 	--exit-after "$exit_after" \
 	2> "/tmp/results/$hostname-c$id.out" &
--- scripts/throughputvslatency.sh
+++ scripts/throughputvslatency.sh
@@ -3,7 +3,7 @@
 run_benchmark() {
 	mkdir -p "$1"
 	ansible-playbook -i scripts/hotstuff.gcp.yml scripts/throughputvslatency.yml \
-		-e "destdir='$1' rate='$2' batch_size='$3' payload='$4' time='$5' num_clients='$6'"
+		-e "destdir='$1' rate='$2' batch_size='$3' payload='$4' max_async='$5' time='$6' num_clients='$7' "
 }
 
 # Exit if anything fails
@@ -13,24 +13,38 @@ basedir="$1/throughputvslatency-$(date +%Y%m%d%H%M%S)"
 mkdir -p "$basedir"
 
 # how many times to repeat each benchmark
-for n in {1..1}; do
+for n in {1..5}; do
 
 	batch=100
 	payload=0
-	for t in {5000,6000,8000,10000,10500,11000,11500,12000,12500,13000}; do
-		run_benchmark "$basedir/lhs-b$batch-p$payload/$n/t$t" "$t" "$batch" "$payload" 10 2
+	for t in {2500,3000,3250,3500,3750,4000,4270,4500,4750,5000,5500}; do
+		run_benchmark "$basedir/lhs-b$batch-p$payload/$n/t$t" "$t" "$batch" "$payload" 75 60 4
+	done
+
+	batch=200
+	payload=0
+	for t in {6000,7000,8000,9000,10000,11000,12000,13000,14000,15000}; do
+		run_benchmark "$basedir/lhs-b$batch-p$payload/$n/t$t" "$t" "$batch" "$payload" 150 60 4
 	done
 
 	batch=400
 	payload=0
-	for t in {40000,50000,60000,70000,80000,90000,100000,110000,120000}; do
-		run_benchmark "$basedir/lhs-b$batch-p$payload/$n/t$t" "$t" "$batch" "$payload" 10 3
+	for t in {10000,15000,20000,25000,27500,30000,32500,35000,37500,40000,42500}; do
+		run_benchmark "$basedir/lhs-b$batch-p$payload/$n/t$t" "$t" "$batch" "$payload" 300 60 4
 	done
 
-	batch=800
-	payload=0
-	for t in {30000,40000,50000,60000,70000,80000,90000,100000,110000,120000,140000}; do
-		run_benchmark "$basedir/lhs-b$batch-p$payload/$n/t$t" "$t" "$batch" "$payload" 10 4
-	done
+	# Payload tests. Need to recompile and redeploy binary
+
+	# batch=200
+	# payload=128
+	# for t in {6000,7000,8000,9000,10000,11000,12000,13000,14000,15000}; do
+	# 	run_benchmark "$basedir/lhs-b$batch-p$payload/$n/t$t" "$t" "$batch" "$payload" 300 60 4
+	# done
+
+	# batch=200
+	# payload=1024
+	# for t in {6000,8000,9000,10000,10500,11000,11250,11500,11750,12000,12500}; do
+	# 	run_benchmark "$basedir/lhs-b$batch-p$payload/$n/t$t" "$t" "$batch" "$payload" 300 60 4
+	# done
 
 done
